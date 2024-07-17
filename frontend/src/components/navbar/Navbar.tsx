0dec98a094a98e920f9b71c259efad42eef53345
Joshf225 <joshuafamose@gmail.com>
2024-07-17 21:29:54 +0100

    fixed errors causing application to crash

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
 
 type NavbarProps = {
@@ -6,38 +6,39 @@ type NavbarProps = {
   navbarHasSelected: Function;
 };
 
-export const Navbar:React.FC<NavbarProps> = (props) => {
-
-  const {categs, navbarHasSelected} = props;
+export const Navbar: React.FC<NavbarProps> = (props) => {
+  const { categs, navbarHasSelected } = props;
 
   const [selectedProduct, setSelectedProduct] = useState("");
 
-
   function createNavbarItems(productsCategories: string[]) {
-    return productsCategories.map((prod, index) => (
+    return productsCategories?.map((prod, index) => (
       <div
-        className={`product ${selectedProduct === prod ? 'selected' : ''}`}
+        className={`product ${selectedProduct === prod ? "selected" : ""}`}
         id={prod + index}
         key={index}
-        onClick={() => {navbarHasSelected(prod) ; setSelectedProduct(prod)}}
+        onClick={() => {
+          navbarHasSelected(prod);
+          setSelectedProduct(prod);
+        }}
       >
         {prod}
       </div>
     ));
   }
 
-
   return (
     <div>
-
-      <div className='navbar'>
-        <div className='iconDiv'>
-          <img src="/src/assets/pictures/knife-fork-nutrition-food-128.svg" alt="fork and knife" width="24" height="24" />
+      <div className="navbar">
+        <div className="iconDiv">
+          <img
+            src="/src/assets/pictures/knife-fork-nutrition-food-128.svg"
+            alt="fork and knife"
+            width="24"
+            height="24"
+          />
         </div>
-        <div className='menu-list'>
-          {createNavbarItems(categs)}
-        </div>
-      
+        <div className="menu-list">{createNavbarItems(categs)}</div>
       </div>
     </div>
   );
--- frontend/src/components/stripe/CheckoutPage.tsx
+++ frontend/src/components/stripe/CheckoutPage.tsx
@@ -1,31 +1,33 @@
-import React, { useCallback } from 'react'
+import React, { useCallback } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   EmbeddedCheckoutProvider,
   EmbeddedCheckout,
 } from "@stripe/react-stripe-js";
+//@ts-ignore
 const stripekey = import.meta.env.VITE_STRIPE_LOAD_KEY;
-if(!stripekey){
-  throw new Error('STRIPE_LOAD_KEY environment variable is not set');
+if (!stripekey) {
+  throw new Error("STRIPE_LOAD_KEY environment variable is not set");
 }
 
 const stripePromise = loadStripe(stripekey);
 export const CheckoutPage = () => {
-    const fetchClientSecret = useCallback(() => {
-        return fetch("http://localhost:8000/create-checkout-session", {
-          method: "POST",
-        })
-          .then((res) => res.json())
-          .then((data) => data.clientSecret);
-      }, []);
-    
-      const options = {fetchClientSecret};
+  const fetchClientSecret = useCallback(() => {
+    return fetch(
+      //@ts-ignore
+      `${import.meta.env.VITE_APP_PUBLIC_SERVER_URL}/create-checkout-session`,
+      {
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => data.clientSecret);
+  }, []);
+
+  const options = { fetchClientSecret };
   return (
-    <EmbeddedCheckoutProvider
-    stripe={stripePromise}
-    options={options}
-  >
-    <EmbeddedCheckout />
-  </EmbeddedCheckoutProvider>
-  )
-}
+    <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      <EmbeddedCheckout />
+    </EmbeddedCheckoutProvider>
+  );
+};
